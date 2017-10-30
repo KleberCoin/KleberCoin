67e2a0ff129a74409194520a7eedc7d7ce66b4e3
Wakedel <nd.alexandre.mansire@gmail.com>
2017-10-30 19:29:01 +0100

    maj

--- KleberCoin.py
+++ KleberCoin.py
@@ -2,21 +2,22 @@ import hashlib
 
 import datetime as date
 
+def sha256(string_to_hash):
+
 class Block:
-    def __init__(self, index, timestamp, data, previous_block):
-        self.index = index
+    def __init__(self, timestamp, data, previous_block):
+        self.index = previous_block.index + 1
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_block.hash
         self.hash = self.hash_block()
 
     def hash_block(self):
-        sha256 = hashlib.sha256()
-        sha256.update(str(self.index) +
+        string_to_hash(str(self.index) +
                         str(self.timestamp) +
                         str(self.data) +
                         str(self.previous_hash))
-        return sha256.hexdigest()
+        return sha256(string_to_hash)
 
 def creation_premier_bloc():
     return Block(0,datetime.datetime.now(),"Premier bloc","0")
