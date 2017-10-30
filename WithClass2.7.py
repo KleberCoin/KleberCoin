fb04bdf2a43f12f5ead7dd4f5a282136b5fdb3d7
Wakedel <nd.alexandre.mansire@gmail.com>
2017-10-31 00:49:30 +0100

    ajout de fonctions
    fonctions ajoutées:
    -vérification de la validité d'un bloc
    -vérification de l'intégrité de la chaîne

--- WithClass2.7.py
+++ WithClass2.7.py
@@ -21,17 +21,28 @@ class Block:
                         str(self.previous_hash))
         return sha256.hexdigest()
 
-def creation_premier_bloc():
-    return Block(0,datetime.datetime.now(),"Premier bloc","0")
+def create_first_block():
+    return Block(0,datetime.datetime.now(),"First Block","0")
 
 
 class Blockchain:
     def __init__(self, first_block):
         self.chain = [first_block]
 
-    def add_block(self, block):
+    def check_block_validity(self, block):
         if block.hash == sha256(str(block.index) +
                             str(block.timestamp) +
                             str(block.data) +
-                            str(self.chain[-1].hash)):
+                            str(self.chain[(block.index - 1].hash)):
+            return True
+        else:
+            return False
+
+    def add_block(self, block):
+        if check_block_validity(block):
             self.chain.append()
+
+    def check_chain_integrity(self):
+        for block in self.chain:
+            if not self.check_block_validity(block):
+                print("Bloc {} not true".format(block.index))
