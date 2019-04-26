56ac3ab3fe18ba667cc9afa3b8d49540b5088fd0
Wakedel <nd.alexandre.mansire@gmail.com>
2019-04-26 11:58:54 +0200

    Update lrgiearfg.py

--- 2019/lrgiearfg.py
+++ 2019/lrgiearfg.py
@@ -17,6 +17,7 @@ class Block:
         self.données = données
         self.marquage_précédent = marquage_précédent
         self.nonce = 0
+        self.cible = 1  # entre 0 et 255 ou 256 ? |mettre dans le sérialiseur
         self.marquage = self.hash()
         
     def hash(self):
@@ -36,10 +37,27 @@ class Block:
     def new(précédent, données):
         return Bloc(données, précédent.index, précédent.marquage)
         
+    def compte_0(self):
+        # On récupère le hash en binaire
+        hash = bin(int(self.marquage, 16))[2:]
+        
+        i = 0
+        while hash[i] == "0":
+            i +=1
+        return i
+    
+    def mine(self):
+        
+        while compte_0() < self.cible:
+            self.nonce += 1
+            self.marquage = self.hash()
     
 class Blockchain:
     def __init__(self, *premiers_blocs):
         self.chaine = [ *premiers_blocs ]
+        
+    def __len__(self):
+        return len( self.chaine )
         
     def vérification_de_chaine(self):
         invalides = []
@@ -55,8 +73,53 @@ class Blockchain:
             bloc.marquage != bloc.hash() or \
             bloc.index != 1 + self.chaine[-1].index :
             return False
-        # C'est moche mais y'a plein d'autres choses à remettre entre
-        else : return True
+        
+        à_vérifier = {}
+        invalides = []
+        valides = []
+    
+        for transaction in bloc.données:
+            if transaction.est_signée() and not transaction.émetteur in invalides:
+                if transaction.émetteur in à_vérifier:
+                    à_vérifier[transaction.émetteur] += - transaction.montant
+                else : à_vérifier[transaction.émetteur] = - transaction.montant
+            else :
+                invalides += [transaction.émetteur]
+        
+        
+        n = (bloc.index - 1)
+        self.vérification_des_transactions(à_vérifier, valides, invalides, n )
+        
+        #print("Valides : ", valides)
+        #print("Invalides : ", invalides )
+        
+        if len(invalides) == 0:
+            return True
+        else : return False
+        
+     # Fonction qui trie les demandes de transfert de fonds contenues dans le
+    # dictionnaire
+    def vérification_des_transactions(self, à_vérifier, valides, invalides, n):
+        """
+            Fonction qui trie les demandes de transferts de fonds contenues 
+            dans le dictionnaire "à_vérifié" en ne considérant la chaîne de
+            blocs que jusqu'au bloc d'inex n.
+            Elle modifie la liste "valides" des émetteurs ayant les fonds
+            nécessaires et la liste "invalides" des émetteurs ne les ayant
+            pas.
+        """
+        for transaction in self.chaine[n].données:
+            if transaction.émetteur in à_vérifier:
+                à_vérifier[transaction.émetteur] -= transaction.montant
+            elif transaction.destinataire in à_vérifier:
+                    à_vérifier[transaction.destinataire] += transaction.montant
+                    if à_vérifier[transaction.destinataire] >= 0:
+                        del(à_vérifier[transaction.destinataire])
+                        valides += [transaction.destinataire]
+                    
+        if len(list(à_vérifier)) !=0 and n > 1:
+            vérification_des_transactions(à_vérifier, valides, invalides, (n-1))
+        else : invalides += list(à_vérifier)
         
     
     def ajout(self, *blocs):
