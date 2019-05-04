78b1458b11b59dd97b8d1e236640d825ba6d72a6
Wakedel <nd.alexandre.mansire@gmail.com>
2019-05-04 19:10:32 +0200

    Update lrgiearfg.py

--- 2019/lrgiearfg.py
+++ 2019/lrgiearfg.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 
+#TO do : fixer un nombre de transactions par bloc pour ne pas inciter à en mettre 0 (moins à calculer pour les hashs) et baiser le système
+"""
+avec max : 
+
+pour synchro : les noeuds peuvent s'échanger le hash de la totalité des trans en att pour se synchro après
+    
+    
+listes globales:
+    totalité transac
+    transactions attente de vérif
+    transactions vérifiées
+    
+penser à l'interface utilisateur:
+    ouverture automatique de l'user à partir d'un .json
+    variables : crédit (qui doit être mis à jour en temps réel)
+    fonctions : envoyer demander vérification avant d'envoyer
+       
+"""
+
 import hashlib
 from Crypto.PublicKey import RSA
 
@@ -11,31 +30,36 @@ def sha256(*data):
     return sha.hexdigest()
 
 class Block:
-    def __init__(self, données, index_précédent, marquage_précédent, date=datetime.datetime.now()):
+    def __init__(self, données, index_précédent, marquage_précédent, cible=0 ,date=datetime.datetime.now()):
         self.index = index_précédent + 1
         self.horodatage = date
         self.données = données
         self.marquage_précédent = marquage_précédent
         self.nonce = 0
-        self.cible = 1  # entre 0 et 255 ou 256 ? |mettre dans le sérialiseur
+        self.cible = cible  # entre 0 et 255 ou 256 ? |mettre dans le sérialiseur
         self.marquage = self.hash()
         
     def hash(self):
+        """ useless comme y'aura pas d'erreurs
         hash_data = ""
         for data in self.données:
             if isinstance(data, Transaction):
                 hash_data = sha256( hash_data + data.hash() + data.signature )
             else:
-                hash_data = sha256( hash_data + str(data) )
-        
+                raise TypeError("Le bloc ne peut contenir que des transactions)
+            """
         return sha256(self.index, 
                       self.horodatage,
-                      hash_data,
+                      self.données,
                       self.marquage_précédent,
                       self.nonce)
-    
-    def new(précédent, données): # prendre la blockchain en paramètre pour avoir la cible du nouveau bloc
+    """
+    def new(précédent, données): 
         return Bloc(données, précédent.index, précédent.marquage)
+    """
+    
+    def next(self, données, cible): 
+        return Block(données, self.index, self.marquage, cible=cible)
         
     def compte_0(self):
         # On récupère le hash en binaire
@@ -51,7 +75,8 @@ class Block:
         while self.compte_0() < self.cible:
             self.nonce += 1
             self.marquage = self.hash()
-    
+            
+            
 class Blockchain:
     def __init__(self, *premiers_blocs):
         self.chaine = [ *premiers_blocs ]
@@ -59,20 +84,23 @@ class Blockchain:
         
     def __len__(self):
         return len( self.chaine )
+    
+    def new_block(self, données):
+        return self.chaine[-1].next(données, self.cible)
         
     def vérification_de_chaine(self):
         invalides = []
         for bloc in self.chaine:
             if not self.vérification_de_bloc(bloc):
-                invalides += bloc.index
+                invalides += [bloc.index]
                 
         return invalides
 
     # Traitement d'un nouveau bloc
     def vérification_de_bloc(self, bloc):
-        if bloc.marquage_précédent != self.chaine[-1].marquage or \
+        if bloc.marquage_précédent != self.chaine[bloc.index - 1].marquage or \
             bloc.marquage != bloc.hash() or \
-            bloc.index != 1 + self.chaine[-1].index :
+            bloc.index != 1 + self.chaine[ bloc.index - 1 ].index :
             return False
         
         à_vérifier = {}
@@ -95,10 +123,13 @@ class Blockchain:
         #print("Invalides : ", invalides )
         
         if len(invalides) == 0:
+            print("Bloc %s valide" % bloc.index)
             return True
-        else : return False
+        else : 
+            print("Bloc %s invalide" % bloc.index)
+            return False
         
-     # Fonction qui trie les demandes de transfert de fonds contenues dans le
+    # Fonction qui trie les demandes de transfert de fonds contenues dans le
     # dictionnaire
     def vérification_des_transactions(self, à_vérifier, valides, invalides, n):
         """
@@ -112,7 +143,7 @@ class Blockchain:
         for transaction in self.chaine[n].données:
             if transaction.émetteur in à_vérifier:
                 à_vérifier[transaction.émetteur] -= transaction.montant
-            elif transaction.destinataire in à_vérifier:
+            if transaction.destinataire in à_vérifier:
                     à_vérifier[transaction.destinataire] += transaction.montant
                     if à_vérifier[transaction.destinataire] >= 0:
                         del(à_vérifier[transaction.destinataire])
@@ -123,16 +154,20 @@ class Blockchain:
         else : invalides += list(à_vérifier)
         
     
-    def ajout(self, *blocs):
-        for bloc in blocs:
-            if self.vérification_de_bloc(bloc):
-                self.chaine.append(bloc)
-            if bloc.index % (14*24*60*6) == 0 :
-                décalage = 14 - (self.chaine[-1].date - self.chaine[bloc.index - 14*24*60*6].date).days
-                if décalage < -7:
-                    self.cible = self.chaine[-1].cible - 1
-                elif décalage > 7:
-                    self.cible = self.chaine[-1].cible - 1
+    def ajout(self, bloc):
+        if self.vérification_de_bloc(bloc) and \
+            self.cible == bloc.compte_0():
+            self.chaine.append(bloc)
+            return True
+        else : return False
+                
+        if bloc.index % (14*24*60*6) == 0 :   #à changer peut être
+            décalage = 14 - (self.chaine[-1].date - self.chaine[bloc.index - 14*24*60*6].date).days
+            if décalage < -7:
+                self.cible = self.chaine[-1].cible - 1
+            elif décalage > 7:
+                self.cible = self.chaine[-1].cible - 1
+
 
 class Transaction:
     def __init__(self, émetteur, destinataire, montant, signature):
